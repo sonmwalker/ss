c00ef1f1ba944f4311b38dc156e7d01e09d4ffd2
deadwalker <zhhzhc0@163.com>
2019-02-05 16:03:10 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -3,35 +3,37 @@
 sudo apt-get install docker.io
 docker run -d -p 12345:12345 oddrationale/docker-shadowsocks -s 0.0.0.0 -p 12345 -k 12345 -m aes-256-cfb
 
-apt-get install build-essential
-wget http://nginx.org/download/nginx-1.7.8.tar.gz
-wget "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.gz"
-wget "https://www.openssl.org/source/openssl-1.0.1j.tar.gz"
-wget "https://www.zlib.net/zlib-1.2.11.tar.gz"
-git clone https://github.com/cuber/ngx_http_google_filter_module
-git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module
+docker run -d -p 80:80 jim3ma/google-mirror
 
-tar -xvpzf nginx-1.7.8.tar.gz
-tar xzvf pcre-8.38.tar.gz
-tar xzvf openssl-1.0.1j.tar.gz
-tar xzvf zlib-1.2.11.tar.gz
+#apt-get install build-essential
+#wget http://nginx.org/download/nginx-1.7.8.tar.gz
+#wget "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.gz"
+#wget "https://www.openssl.org/source/openssl-1.0.1j.tar.gz"
+#wget "https://www.zlib.net/zlib-1.2.11.tar.gz"
+#git clone https://github.com/cuber/ngx_http_google_filter_module
+#git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module
 
-cd nginx-1.7.8
-./configure \
-  --prefix=/opt/nginx-1.7.8 \
-  --with-pcre=../pcre-8.38 \
-  --with-openssl=../openssl-1.0.1j \
-  --with-zlib=../zlib-1.2.11 \
-  --with-http_ssl_module \
-  --add-module=../ngx_http_google_filter_module \
-  --add-module=../ngx_http_substitutions_filter_module
+#tar -xvpzf nginx-1.7.8.tar.gz
+#tar xzvf pcre-8.38.tar.gz
+#tar xzvf openssl-1.0.1j.tar.gz
+#tar xzvf zlib-1.2.11.tar.gz
 
-make install 
+#cd nginx-1.7.8
+#./configure \
+#  --prefix=/opt/nginx-1.7.8 \
+#  --with-pcre=../pcre-8.38 \
+#  --with-openssl=../openssl-1.0.1j \
+#  --with-zlib=../zlib-1.2.11 \
+#  --with-http_ssl_module \
+#  --add-module=../ngx_http_google_filter_module \
+#  --add-module=../ngx_http_substitutions_filter_module
 
-cd /opt/nginx-1.7.8/conf
+#make install 
 
-mkdir vhost
-cd vhost
+#cd /opt/nginx-1.7.8/conf
+
+#mkdir vhost
+#cd vhost
 echo "server {
   # ... part of server configuration
   server_name 45.76.157.234;
